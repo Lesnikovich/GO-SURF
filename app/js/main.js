538753ed249cdedcd6eb00aad79011283368aaba
Lesnikovich <68425826+Lesnikovich@users.noreply.github.com>
2023-12-10 17:58:34 +0200

    Header with slider

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,7 @@ $(function(){
     fade: true,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="Left arrow"></img>',
     nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="Right arrow"></img>',
-    asNavFor: '.slider-dots',
+    asNavFor: '.slider-dots, .header__map',
   });
 
   $('.slider-dots').slick({
@@ -14,4 +14,11 @@ $(function(){
     asNavFor: '.header__slider',
   });
 
+  $('.header__map').slick({
+    arrows: false,
+    fade: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.header__slider',
+  });
 });
