b7d9b70276368592b9a0f79772ba0f5cb646a829
Lesnikovich <68425826+Lesnikovich@users.noreply.github.com>
2023-12-28 00:52:17 +0200

    Block Travel has been completed

--- app/js/main.js
+++ app/js/main.js
@@ -45,4 +45,11 @@ $(function(){
     focusOnSelect: true,
   });
 
+  $('.travel__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="Left arrow"></img>',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="Right arrow"></img>',
+  });
+
 });
